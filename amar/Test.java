bfd0ca0eddd6fe23c7e6562748af78205523fafe
Amarnath.C <c.amarnath236@gmail.com>
2025-03-28 09:20:33 +0530

    first commit count of characters

--- amar/Test.java
+++ amar/Test.java
@@ -1,4 +1,41 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Test {
 
+        public static String countCharacters(String s) {
 
-}
+            Map<Character, Integer> charCountMap = new LinkedHashMap<>();
+
+            // Count occurrences of each character
+
+            for (char c : s.toCharArray()) {
+
+                charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+
+            }
+
+
+
+
+            StringBuilder result = new StringBuilder();
+
+            for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+
+                result.append(entry.getKey()).append(entry.getValue());
+
+            }
+
+            return result.toString();
+
+        }
+
+        public static void main(String[] args) {
+
+            String s = "aababbccde";
+
+            System.out.println("Output: " + countCharacters(s));
+
+        }
+
+    }
